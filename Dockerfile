026d1f1387d9c446840ece10690390ec87de17b6
Benjamin DeCoste <bendecoste@gmail.com>
2022-10-14 10:41:44 -0300

    Listen over vsock on port 2345

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM golang:1.18-alpine
 WORKDIR /app
 
 COPY go.mod ./
+COPY go.sum ./
 RUN go mod download
 COPY *.go ./
 
--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"io"
 	"log"
 	"net/http"
-)
 
-const PORT = ":8888"
+	"github.com/mdlayher/vsock"
+)
 
 func main() {
 	handler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, Enclave!\n")
 	}
 
-	http.HandleFunc("/", handler)
-	log.Println("listening on", PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	listener, err := vsock.Listen(2345, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(http.Serve(listener, http.HandlerFunc(handler)))
 }
